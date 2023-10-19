54429078eb892c816bdaa04f41802fb65daa2931
abcdato <42653648+abcdato@users.noreply.github.com>
2023-10-19 18:40:07 +0300

    fix: clean up context component due to state movement to other components

--- src/components/Context/DataContext.jsx
+++ src/components/Context/DataContext.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable import/no-cycle */
 import { createContext, useState } from 'react';
-import { Alert, Spin } from 'antd';
 import MovieService from '../../api/MovieService';
 import MovieCard from '../MovieCard/MovieCard';
 
@@ -12,19 +11,6 @@ function DataProvider({ children }) {
   const movieService = new MovieService();
 
   const [genreList, setGenreList] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const [ratedMovies, setRatedMovies] = useState([]);
-  const [totalRatedPages, setTotalRatedPages] = useState(null);
-
-  const handleError = (message) => {
-    setLoading(false);
-    setError(true);
-    setErrorMessage(message);
-  };
 
   const showMovies = (data) =>
     data &&
@@ -60,28 +46,6 @@ function DataProvider({ children }) {
       );
     });
 
-  async function loadRatedData(page = 1) {
-    try {
-      const data = movieService.getRatedMovies(page);
-      const rated = await data;
-
-      if (!rated.totalResults) {
-        handleError("Unfortunately we couldn't find any movies");
-      } else if (rated.totalResults) {
-        setRatedMovies(rated.movies);
-        setTotalRatedPages(rated.totalPages);
-        setLoading(false);
-        setError(false);
-      } else if (!rated) {
-        setRatedMovies([]);
-        setLoading(false);
-        setError(false);
-      }
-    } catch {
-      handleError("Couldn't load the data.");
-    }
-  }
-
   async function getGenres() {
     const allGenres = movieService.getGenres();
     const genres = await allGenres;
@@ -89,26 +53,9 @@ function DataProvider({ children }) {
     setGenreList(genres.genres);
   }
 
-  const spinner = loading ? <Spin size="large" /> : null;
-  const errorMsg = error ? (
-    <Alert message="Error" description={errorMessage} type="warning" showIcon />
-  ) : null;
-
-  const ratedContent = showMovies(ratedMovies);
-
   const value = {
     showMovies,
     getGenres,
-    setLoading,
-    handleError,
-    setError,
-    loadRatedData,
-    loading,
-    spinner,
-    error,
-    errorMsg,
-    ratedContent,
-    totalRatedPages,
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
